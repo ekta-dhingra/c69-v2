badb93f665cd0d6dd6d7d15da9e6565f459a5f06
ekta-dhingra <70560917+ekta-dhingra@users.noreply.github.com>
2021-07-28 18:03:02 +0530

    Add files via upload

---
+++ screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class SearchScreen extends React.Component{
+    render(){
+        return(
+        <View>
+            <Text> Search Screen!</Text>
+        </View>
+        )
+    }
+}
